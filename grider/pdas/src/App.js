3ab7f4ad71313c21dc017e0bf666b3970bf2c164
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-07 10:41:21 +0300

    Completed the project on PDAs

--- grider/pdas/src/App.js
+++ grider/pdas/src/App.js
@@ -1,12 +1,42 @@
+import 'bulma/css/bulma.css';
+
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App(){
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cprtane" handle="@cortana32" />
-            <ProfileCard title="Siri" handle="@siri01" />
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard title="Alexa" 
+                                         handle="@alexa99" 
+                                         image={AlexaImage}
+                                         description="Alexa was created by Amazon and helps you buy things" />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title="Cortana" 
+                                         handle="@cortana32" 
+                                         image={CortanaImage} 
+                                         description="Cortana was made by Microsoft. Who knows what it does?" />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title="Siri" 
+                                         handle="@siri01" 
+                                         image={SiriImage}
+                                         description="Siri was made Apple and is being phased out." />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
--- grider/pdas/src/ProfileCard.js
+++ grider/pdas/src/ProfileCard.js
@@ -1,8 +1,22 @@
-function ProfileCard(props){
+function ProfileCard({ title, handle, image, description }){
     return (
-        <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="PDA logo" />
+                </figure>
+            </div>
+            
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">Title is {title}</p>
+                    <p className="subtitle is-6">Handle is {handle}</p>
+                </div>
+                <div className="content">
+                    {description}
+                </div>
+            </div>
+            
         </div>
     );
 }
