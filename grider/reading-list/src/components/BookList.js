56a10702ef253899165d399f0cc47fb82a56e510
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-22 12:47:32 +0800

    Started refactoring the App to use Context

--- grider/reading-list/src/components/BookList.js
+++ grider/reading-list/src/components/BookList.js
@@ -4,15 +4,12 @@ import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 const BookList = ({ books, onDelete, onEdit }) => {
-  const { count, incrementCount } = useContext(BooksContext);
   
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     });
   return(
     <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
       {renderedBooks}
     </div>
 
--- grider/reading-list/src/context/books.js
+++ grider/reading-list/src/context/books.js
@@ -1,17 +1,56 @@
 import { createContext, useState } from "react";
+import axios from 'axios';
 
 const BooksContext = createContext();
 
 function Provider({ children }){
-  const [count, setCount] = useState(5);
-
-  const valueToShare = {
-    count,
-    incrementCount: () => {
-      setCount(count + 1);
-    },
+  const [books, setBooks] = useState([]);
+  
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
   };
-  return <BooksContext.Provider value={valueToShare}>
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
+    // console.log(response);
+
+    const updatedBooks = books.map((book) => {
+      if(book.id === id){
+        return { ...book, ...response.data };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    });
+    
+    const updatedBooks = [
+      ...books,
+      response.data
+    ];
+    setBooks(updatedBooks);
+  }
+
+
+  return <BooksContext.Provider value={{}}>
     {children}
   </BooksContext.Provider>
 }
