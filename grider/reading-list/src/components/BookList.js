937aafafe6cfca1b40f088b424952c641ff49ff2
Vladimir Solovyov <leilyngale@icloud.com>
2022-12-22 12:06:21 +0800

    Using context

--- grider/reading-list/src/components/BookList.js
+++ grider/reading-list/src/components/BookList.js
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
+
 import BookShow from "./BookShow";
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const { count, incrementCount } = useContext(BooksContext);
+  
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     });
   return(
     <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click</button>
       {renderedBooks}
     </div>
 
--- grider/reading-list/src/index.js
+++ grider/reading-list/src/index.js
@@ -4,11 +4,15 @@ import App from "./App";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from './context/books';
+
 const elt = document.getElementById('root');
 const root = ReactDOM.createRoot(elt);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
   );
