68d02ba029000fdd80634bcbb018f133f3accc5d
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-13 11:46:23 +0800

    Passing button text via children prop

--- grider/components-showdown/src/App.js
+++ grider/components-showdown/src/App.js
@@ -1,5 +1,22 @@
+import Button from "./components/Button";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>First Button</Button>
+      </div>
+      <div>
+       <Button>Second Button</Button>
+      </div>
+      <div>
+        <Button>Third Button</Button>
+      </div>
+      <div>
+          <Button>Fourt Button</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
