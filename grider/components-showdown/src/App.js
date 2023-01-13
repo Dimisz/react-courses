d93c6ce2c7e9c5a92e034031e5807746dc3b1a83
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-13 12:42:30 +0800

    Validating props with prop types

--- grider/components-showdown/src/App.js
+++ grider/components-showdown/src/App.js
@@ -4,16 +4,16 @@ const App = () => {
   return (
     <div>
       <div>
-        <Button>First Button</Button>
+        <Button success>First Button</Button>
       </div>
       <div>
-       <Button>Second Button</Button>
+       <Button danger>Second Button</Button>
       </div>
       <div>
-        <Button>Third Button</Button>
+        <Button primary>Third Button</Button>
       </div>
       <div>
-          <Button>Fourt Button</Button>
+          <Button warning>Fourt Button</Button>
       </div>
     </div>
   );
--- grider/components-showdown/src/components/Button.js
+++ grider/components-showdown/src/components/Button.js
@@ -1,7 +1,32 @@
-const Button = ({children}) => {
+import PropTypes from 'prop-types';
+
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) => {
   return (
     <button>{children}</button>
   );
 }
+
+Button.propTypes = {
+  checkVariationValue: ({primary, secondary, success, warning, danger}) => {
+    const count = Number(!!primary) 
+      + Number(!!secondary)
+      + Number(!!warning)
+      + Number(!!success)
+      + Number(!!danger)
+
+    if(count > 1){
+      return new Error('Only one variation - primary, secondary, success, warning, danger - can be true');
+    }
+  }
+};
 
 export default Button;
