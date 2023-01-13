ba6055d71d6919045633a70d4d33d3cb9aadc723
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-13 16:11:29 +0800

    Styled the buttons

--- grider/components-showdown/src/App.js
+++ grider/components-showdown/src/App.js
@@ -4,16 +4,19 @@ const App = () => {
   return (
     <div>
       <div>
-        <Button success>First Button</Button>
+        <Button success rounded>First Button</Button>
       </div>
       <div>
-       <Button danger>Second Button</Button>
+       <Button danger outline>Second Button</Button>
       </div>
       <div>
-        <Button primary>Third Button</Button>
+        <Button primary rounded outline>Third Button</Button>
       </div>
       <div>
-          <Button warning>Fourt Button</Button>
+          <Button warning rounded>Fourt Button</Button>
+      </div>
+      <div>
+          <Button secondary>Fourt Button</Button>
       </div>
     </div>
   );
--- grider/components-showdown/src/components/Button.js
+++ grider/components-showdown/src/components/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import className from "classnames";
 
 const Button = ({
   children,
@@ -10,8 +10,23 @@ const Button = ({
   outline,
   rounded
 }) => {
+  const classes = className('px-3 py-1.5 border', {
+    'border-blue-500 bg-blue-500 text-white': primary,
+    'border-gray-900 bg-gray-900 text-white': secondary,
+    'border-green-500 bg-green-500 text-white': success,
+    'border-yellow-400 bg-yellow-400 text-white': warning,
+    'border-red-500 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white': outline,
+    'text-blue-500': outline && primary,
+    'text-gray-900': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-400': outline && warning,
+    'text-red-500': outline && danger
+
+  });
   return (
-    <button className='px-3 py-1.5 border border-blue-600 bg-blue-500 text-white'>{children}</button>
+    <button className={classes}>{children}</button>
   );
 }
 
