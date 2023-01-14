b5eb9f74acbc26d22c23a3b685847aea6104c03e
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-14 18:03:49 +0800

    Styled the accordion

--- grider/components-showdown/src/App.js
+++ grider/components-showdown/src/App.js
@@ -5,17 +5,17 @@ import Accordion from './pages/Accordion';
 const App = () => {
   const items = [
     {
-      id: 'l23dsvd',
+      id: 0,
       label: 'Can I use React on a project',
       content: 'You can use React on any project you want'
     }, 
     {
-      id: 'csdvafv',
+      id: 1,
       label: 'Can I use JS on a project',
       content: 'You can use JS on any project you want'
     }, 
     {
-      id: 'mkmkni',
+      id: 2,
       label: 'Can I use CSS on a project',
       content: 'You can use CSS on any project you want'
     }, 
--- grider/components-showdown/src/pages/Accordion.js
+++ grider/components-showdown/src/pages/Accordion.js
@@ -1,20 +1,22 @@
 import { useState } from "react";
-
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 const Accordion = ({items}) => {
   const [expandedIndex, setExpandedIndex] = useState(1);
 
+  
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
+    const icon = <span className="text-2xl">{isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}</span>;
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{isExpanded && <div>{item.content}</div>}</div>
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => setExpandedIndex(index)}>{item.label}{icon}</div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
 
     );
   })
   return (
-    <div>
+    <div className="border-x border-t rounded">
       {renderedItems}
     </div>
   );
