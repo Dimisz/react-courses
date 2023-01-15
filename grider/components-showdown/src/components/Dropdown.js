169f5093e2465a58510c3a07957c0ae586def20d
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-15 09:17:04 +0800

    Event handlers for the dropdown

--- grider/components-showdown/src/components/Dropdown.js
+++ grider/components-showdown/src/components/Dropdown.js
@@ -2,21 +2,31 @@ import { useState } from 'react';
 
 const Dropdown = ({ options }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [selectedValue, setSelectedValue] = useState('Select...');
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    setIsOpen((currentIsOpen) => {
+      return !currentIsOpen;
+    });
+  };
+
+  const handleOptionClick = (value) => {
+    console.log('I should close');
+    setSelectedValue(value);
+    setIsOpen((currentIsOpen) => {
+      return !currentIsOpen;
+    });
   };
 
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>
+    return <div onClick={() => handleOptionClick(option.value)} key={option.value}>
       {option.label}
     </div>
   });
 
   return (
     <div>
-      <div onClick={handleClick}>Select...</div>
-      <div>{isOpen && renderedOptions}</div>
+      <div onClick={handleClick}>{selectedValue}</div>
+      {isOpen &&  <div>{renderedOptions}</div>}
     </div>
   );
 }
