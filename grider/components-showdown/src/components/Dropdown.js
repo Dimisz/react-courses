edaf89e5ab0732f4809f8dc8f6ab1ab1e3d720b7
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-15 10:13:01 +0800

    Added styling and icons

--- grider/components-showdown/src/components/Dropdown.js
+++ grider/components-showdown/src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft }from 'react-icons/go';
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,15 +18,20 @@ const Dropdown = ({ options, value, onChange }) => {
   };
 
   const renderedOptions = options.map((option) => {
-    return <div onClick={() => handleOptionClick(option.label)} key={option.value}>
+    return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option.label)} key={option.value}>
       {option.label}
     </div>
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value || "Select..."}</div>
-      {isOpen &&  <div>{renderedOptions}</div>}
+    <div className='w-48 relative'>
+      <div className="flex justify-between items-center cursor-pointer border rounder p-3 shadow bg-white w-full" 
+           onClick={handleClick}
+      >
+        {value || "Select..."}
+        {isOpen ? <GoChevronDown className='text-lg'/> : <GoChevronLeft className='text-lg'/>}
+      </div>
+      {isOpen &&  <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
     </div>
   );
 }
