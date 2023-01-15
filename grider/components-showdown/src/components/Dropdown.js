3b00af2886fb18ed9645d1daf8ea6d8af11fb896
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-15 08:54:57 +0800

    Added barebone dropdown functionality

--- grider/components-showdown/src/components/Dropdown.js
+++ grider/components-showdown/src/components/Dropdown.js
@@ -1,6 +1,23 @@
+import { useState } from 'react';
+
 const Dropdown = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>
+      {option.label}
+    </div>
+  });
+
   return (
-    <div>Dropdown</div>
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      <div>{isOpen && renderedOptions}</div>
+    </div>
   );
 }
 
