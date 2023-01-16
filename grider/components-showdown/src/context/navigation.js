9fb340bace9d803e50d6d7d356a89eabfce294ca
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-16 14:13:23 +0800

    Handled forward and backward button navigation

---
+++ grider/components-showdown/src/context/navigation.js
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect } from "react";
+
+const NavigationContext = createContext();
+
+const NavigationProvider = ({children}) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener('popstate', handler);
+
+    // cleanup
+    window.removeEventListener('popstate', handler);
+  }, []);
+
+  return <NavigationContext.Provider value={{}}>
+    {currentPath}
+    {children}
+  </NavigationContext.Provider>
+}
+
+export { NavigationProvider };
+export default NavigationContext;
--- grider/components-showdown/src/index.js
+++ grider/components-showdown/src/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { NavigationProvider } from './context/navigation';
 const elt = document.getElementById('root');
 const root = ReactDOM.createRoot(elt);
 
-root.render(<App/>);
+root.render(
+  <NavigationProvider>
+    <App/>
+  </NavigationProvider>
+);
