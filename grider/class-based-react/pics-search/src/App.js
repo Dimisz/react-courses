e95decfeafe4cfbff71a164d92fa201ac277cdae
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-18 20:37:30 +0800

    Used axios to fetch pictures from unsplash

--- grider/class-based-react/pics-search/src/App.js
+++ grider/class-based-react/pics-search/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-  }
+  state = { images: [] };
 
+  onSerachSubmit = async(term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos',{
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID acces-key'
+      }
+    });
+    //console.log(response.data.results);
+    this.setState({ images: response.data.results });
+    
+  }
   render(){
     return(
       <div className='ui container' style={{marginTop: '10px'}}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSerachSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
--- grider/class-based-react/pics-search/src/components/SearchBar.js
+++ grider/class-based-react/pics-search/src/components/SearchBar.js
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
     this.setState({term: ''});
   }
   render(){
