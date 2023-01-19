771d6997c8597f4faa2ca261f62ab5406cebe527
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-19 11:42:09 +0800

    Initial cleanup after create-react-app

--- grider/class-based-react/youtube-search-app/src/App.js
+++ grider/class-based-react/youtube-search-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import SearchBar from './components/SearchBar';
+
 class App extends React.Component {
   render(){
     return(
-      <h1>Youtube search app.</h1>
+      <div>
+        <h1>Youtube search app.</h1>
+        <SearchBar />
+      </div>
     );
   }
 }
