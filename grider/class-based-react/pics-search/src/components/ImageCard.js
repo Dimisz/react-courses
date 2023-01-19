2ef5cb2e1ef6d8255c09952a81c1bf4afa41297a
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-19 10:57:11 +0800

    Completed the layout

--- grider/class-based-react/pics-search/src/components/ImageCard.js
+++ grider/class-based-react/pics-search/src/components/ImageCard.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  // setting height of images
+  componentDidMount(){
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({spans});
+  } // setting height & width
+
   render(){
     const {description, urls} = this.props.image;
     return(
-      <div>
-        <img alt={description} src={urls.regular}/>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+        <img ref={this.imageRef} alt={description} src={urls.regular}/>
       </div>
     );
   }
