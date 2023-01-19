f0e1fc401d16224bc40d143862278998ffabd20d
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-19 15:53:31 +0800

    Saving fetched videos in state var in App

--- grider/class-based-react/youtube-search-app/src/App.js
+++ grider/class-based-react/youtube-search-app/src/App.js
@@ -4,6 +4,7 @@ import SearchBar from './components/SearchBar';
 import youtubeApi from './api/youtubeApi';
 
 class App extends React.Component {
+  state = { videos: [] };
 
   onFormSubmit = async (term) => {
     const response = await youtubeApi.get('/search', {
@@ -11,13 +12,14 @@ class App extends React.Component {
         q: term
       }
     });
-    console.log(response.data.items.length);
+    this.setState({ videos: response.data.items });
   };
 
   render(){
     return(
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onFormSubmit} />
+        <p>I have {this.state.videos.length} videos.</p>
       </div>
     );
   }
