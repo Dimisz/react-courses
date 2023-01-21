fb603444e4e310025224eba149b5cf463cdcefc3
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-21 12:54:51 +0800

    Scaffolded the Accordion component

--- grider/class-based-react/components-library/src/App.js
+++ grider/class-based-react/components-library/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React a front-end JS library'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite library among engineers'
+  },
+  {
+    title: 'How to use React?',
+    content: 'React is used by creating components'
+  }
+];
+
 const App = () => {
   return(
-    <h1>App Page</h1>
+    <Accordion items={items} />
   );
 }
 
---
+++ grider/class-based-react/components-library/src/components/Accordion.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return( 
+    <div key={item.title}>
+      <div className='title active'>
+        <i className='dropdown icon'></i>
+        {item.title}
+      </div>
+      <div className='content active'>
+        <p>{item.content}</p>
+      </div>
+    </div>);
+  })
+  return(
+    <div className='ui styled accordion'>
+      {renderedItems}
+    </div>
+  );
+}
+
+export default Accordion;
