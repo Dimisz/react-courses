be4a3f73204049d764e28a553cabec3937768a4b
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-21 08:01:53 +0800

    Scaffolded the app

---
+++ grider/class-based-react/components-library/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const elt = document.getElementById('root');
+const root = ReactDOM.createRoot(elt);
+
+root.render(<App />);
