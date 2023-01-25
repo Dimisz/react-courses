0d438be6dcfc87532dc434720569d8f705cbe5f6
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-25 10:51:44 +0800

    Implemented navigation from scratch

--- grider/class-based-react/components-library/src/App.js
+++ grider/class-based-react/components-library/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import Route from './components/Route';
 
+import Accordion from './components/Accordion';
 import Translate from './components/Translate';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const options = [
   {
@@ -30,11 +34,54 @@ const items = [
     content: 'React is used by creating components'
   }
 ];
+// const showAccordion = () => {
+//   if(window.location.pathname === '/'){
+//     return <Accordion items={items} />;
+//   }
+// }
+
+// const showSearch = () => {
+//   if(window.location.pathname === '/search'){
+//     return <Search />;
+//   }
+// }
+
+// const showDropdown = () => {
+//   if(window.location.pathname === '/dropdown'){
+//     return (
+//             <Dropdown />);
+//   }
+// }
+
+// const showTranslate = () => {
+//   if(window.location.pathname === '/translate'){
+//     return (
+//             <Translate />);
+//   }
+// }
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  
   return(
     <div>
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown label="Select a color"
+                  options={options}
+                  selected={selected}
+                  onSelectedChange={setSelected}
+        
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
