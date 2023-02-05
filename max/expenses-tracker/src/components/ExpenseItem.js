c3c90d0ff7170f43a885838cafcac441e95f8134
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-05 20:42:17 +0800

    Added styling to the expense component

--- max/expenses-tracker/src/components/ExpenseItem.js
+++ max/expenses-tracker/src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import './ExpenseItem.css';
+
 const ExpenseItem = () => {
   return(
-    <h2>Expense item...</h2>
+    <div className='expense-item'>
+      <div>March 28th 2021</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$284.67</div>
+      </div>
+    </div>
   );
 }
 
