32fc2eadd90f4998895c50b68fd7cf621368de22
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-06 12:53:08 +0800

    Wrapped the expense items into Card component for styling purposes

--- max/expenses-tracker/src/components/ExpenseItem.js
+++ max/expenses-tracker/src/components/ExpenseItem.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './ExpenseItem.css';
 
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 const ExpenseItem = ({ expense }) => {
 
   return(
-    <div className='expense-item'>
-      <ExpenseDate date={expense.date} />
-      <div className='expense-item__description'>
-        <h2>{expense.title}</h2>
-        <div className='expense-item__price'>${expense.amount}</div>
-      </div>
-    </div>
+
+      <Card className='expense-item'>
+        <ExpenseDate date={expense.date} />
+        <div className='expense-item__description'>
+          <h2>{expense.title}</h2>
+          <div className='expense-item__price'>${expense.amount}</div>
+        </div>
+      </Card>
   );
 }
 
--- max/expenses-tracker/src/components/Expenses.js
+++ max/expenses-tracker/src/components/Expenses.js
@@ -1,15 +1,16 @@
 import './Expenses.css';
 
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 const Expenses = ({expenses}) => {
   const renderedExpenses = expenses.map((expense) => <ExpenseItem expense={expense} key={expense.id}/>);
 
   return(
-    
-    <div className='expenses'>{renderedExpenses}</div>
+   
+      <Card className='expenses'>{renderedExpenses}</Card>
+ 
   );
-
 }
 
 export default Expenses;
