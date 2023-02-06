dcba47858da14533784a24a50b1d2f429b10df84
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-06 21:46:46 +0800

    Created Navigation Provider

--- grider/components-showdown/src/index.js
+++ grider/components-showdown/src/index.js
@@ -2,12 +2,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { NavigationProvider } from './context/navigation';
 
 const elt = document.getElementById('root');
 const root = ReactDOM.createRoot(elt);
 
 root.render(
-  
-    <App/>
-  
+    <NavigationProvider>   
+        <App/> 
+    </NavigationProvider>
 );
