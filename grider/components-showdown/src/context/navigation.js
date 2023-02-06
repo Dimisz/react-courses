a5bc4e0fd121fe114352968553e6280747a2a5dc
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-06 22:12:38 +0800

    Implemented navigate function

--- grider/components-showdown/src/context/navigation.js
+++ grider/components-showdown/src/context/navigation.js
@@ -1,10 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState, ueEffect, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({children}) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handler);
+
+    return () => {
+      window.removeEventListener('popstate', handler);
+    };
+  }, []);
+
+  const navigate = (to) => {
+    window.history.pushState({}, '', to);
+    setCurrentPath(to);
+  };
+
   return(
     <NavigationContext.Provider value={{}}>
+      <div>
+        <button onClick={() => navigate('/accordion')}>Go to accordion</button>
+        <button onClick={() => navigate('/dropdown')}>Go to dropdown</button>
+      </div>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
