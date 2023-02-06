da84d98a5384a2dae79c545b2bedda47bf69ec19
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-06 22:55:24 +0800

    Created Route component

--- grider/components-showdown/src/App.js
+++ grider/components-showdown/src/App.js
@@ -1,12 +1,26 @@
 import Link from "./components/Link";
+import Route from "./components/Route";
+
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
 
 const App = () => {
 
   return (
-    <div>
-      <Link to='/accordion'>Go to accordion</Link>
-      <Link to='/dropdown'>Go to dropdown</Link>
-    </div>
+    <>
+      <div>
+        <Link to='/accordion'>Go to accordion</Link>
+        <Link to='/dropdown'>Go to dropdown</Link>
+      </div>
+      <div>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/dropdown'>
+          <DropdownPage />
+        </Route>
+      </div>
+    </>
   );
 }
 
