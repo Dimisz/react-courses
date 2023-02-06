0c023e28a39ccae2312a6e3e0bbfa3bd7b6e4718
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-06 23:08:35 +0800

    Created a custom hook for navigation functionality

--- grider/components-showdown/src/components/Link.js
+++ grider/components-showdown/src/components/Link.js
@@ -1,16 +1,27 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+
+// import { useContext } from "react";
+// import NavigationContext from "../context/navigation";
+// replaced by a custom hook below
+
+import useNavigation from "../hooks/useNavigation";
 
 const Link = ({ to, children }) => {
-  const { navigate }  = useContext(NavigationContext);
+  // const { navigate }  = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+  
+  const classes = classNames('text-blue-500');
 
   const handleClick = (event) => {
+    if(event.metaKey || event.ctrlKey){
+      return;
+    }
     event.preventDefault();
     navigate(to);
   };
 
   return(
-    <a onClick={handleClick}>{children}</a>
+    <a className={classes} href={to} onClick={handleClick}>{children}</a>
   );
 }
 
--- grider/components-showdown/src/components/Route.js
+++ grider/components-showdown/src/components/Route.js
@@ -1,13 +1,17 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+// import { useContext } from "react";
+// import NavigationContext from "../context/navigation";
+// replaced by custom hook below
+
+import useNavigation from "../hooks/useNavigation";
 
 const Route = ({ path, children }) => {
-  const { currentPath } = useContext(NavigationContext);
-
+  //const { currentPath } = useContext(NavigationContext);
+  const { currentPath } = useNavigation();
+  
   if(path === currentPath){
     return children;
   }
-  
+
   return null;
 }
 
