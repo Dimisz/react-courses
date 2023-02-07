e3da93b8dccadd71a0c9962f4bae79a9857433d9
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-07 12:59:39 +0800

    Applied styling

--- grider/components-showdown/src/components/Table.js
+++ grider/components-showdown/src/components/Table.js
@@ -1,6 +1,27 @@
-const Table = () => {
+const Table = ({ data }) => {
   return(
-    <div>Table component</div>
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruits</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((fruit) => {
+          return(
+            <tr className="border-b" key={fruit.name}>
+              <td className="p-3">{fruit.name}</td>
+              <td className='p-3'>
+                <div className={`p-3 m-2 ${fruit.color}`}></div>
+              </td>
+              <td className="p-3">{fruit.score}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   );
 }
 
--- grider/components-showdown/src/pages/TablePage.js
+++ grider/components-showdown/src/pages/TablePage.js
@@ -1,9 +1,15 @@
 import Table from "../components/Table";
 
 const TablePage = () => {
+  const data = [
+    { name: 'Orange', color: 'bg-orange-500', score: 5 },
+    { name: 'Apple', color: 'bg-red-500', score: 3 },
+    { name: 'Banana', color: 'bg-green-500', score: 1 },
+    { name: 'Lime', color: 'bg-lime-500', score: 4 },
+  ];
   return (
   <div>
-    <Table />
+    <Table data={data}/>
   </div>
   );
 }
