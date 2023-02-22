25279f6df675d06eba60df943b973d1fb7e2c5fb
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-22 20:58:24 +0800

    Styled the date component

--- max/expenses-tracker/src/components/Date.js
+++ max/expenses-tracker/src/components/Date.js
@@ -1,13 +1,14 @@
+import './Date.css'
 const Date = ({date}) => {
   
   const day = date.toLocaleString('en-US', {day: '2-digit'} )
   const month = date.toLocaleString('en-US', { month: 'short' });
   const year = date.getFullYear();
   return(
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
     </div>
   )
 }
