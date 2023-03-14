4c61c9b198ac892e5697f7cf210a47e746f63d25
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-14 11:28:40 +0800

    Arrow functions preferred to manage set
    State

---
+++ colt/state-clicker/src/Counter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  decrement = () => {
+    this.setState({count: this.state.count - 1});
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
