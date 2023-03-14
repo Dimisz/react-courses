9c7aaa96d35977c6e8afd2b1f56e47ff092b8482
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-14 15:51:55 +0800

    Added a clicker

--- colt/state-clicker/src/Counter.js
+++ colt/state-clicker/src/Counter.js
@@ -3,25 +3,23 @@ import React from 'react';
 class Counter extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      count: 0
-    };
+    this.state = { randomNumber: Math.floor(Math.random() * 10) + 1 };
+    this.generateRandomNumber = this.generateRandomNumber.bind(this);
   }
 
-  increment = () => {
-    this.setState({count: this.state.count + 1});
+  generateRandomNumber(){
+    this.setState({ randomNumber: Math.floor(Math.random() * 10) + 1 });
   }
-
-  decrement = () => {
-    this.setState({count: this.state.count - 1});
-  }
+  
 
   render(){
+    const btn = <button onClick={this.generateRandomNumber}>Generate random number</button>;
+    const winnerMessage = <h2>YOU WIN!!!</h2>;
+
     return(
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
+      <div style={{fontFamily: 'sans-serif', textAlign:'center'}}>
+        <h1>Your random number: {this.state.randomNumber}</h1>
+        { this.state.randomNumber === 7 ? winnerMessage : btn }
       </div>
     )
   }
