20134316377a8666ab26c3f95170de4cc9e8be47
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-15 10:23:00 +0800

    Three ways of setting state

--- colt/state-examples/src/components/ScoreKeeper.js
+++ colt/state-examples/src/components/ScoreKeeper.js
@@ -6,18 +6,38 @@ class ScoreKeeper extends React.Component {
     this.state = {
       score: 0
     }
-    this.increment = this.increment.bind(this);
+    this.incrementByOne = this.incrementByOne.bind(this);
+    this.incrementByThree = this.incrementByThree.bind(this);
   }
 
-  increment(){
+  //1.  SIMPLE WAY, BETTER TO AVOID AT TIMES
+  incrementByOne(){
     this.setState({score: this.state.score + 1});
+  }
+
+  // incrementByThree(){
+  //   this.setState({score: this.state.score + 3});
+  // }
+
+  //2.  CALLBACK FORM, PREFERRED WAY
+  // incrementByThree(){
+  //   this.setState((st) => {return {score: st.score + 3}});
+  // }
+
+  //3. CALLBACK FORM EXTRACTING THE CALLBACK INTO A SEPARATE FUNCTION
+  incrementScore(curState){
+    return {score: curState.score + 2};
+  }
+  incrementByThree(){
+    this.setState(this.incrementScore);
   }
 
   render(){
     return(
       <>
         <h1>Score is: {this.state.score}</h1>
-        <button onClick={this.increment}>Add Score</button>
+        <button onClick={this.incrementByOne}>Add One</button>
+        <button onClick={this.incrementByThree}>Add Three</button>
       </>
     )
   }
