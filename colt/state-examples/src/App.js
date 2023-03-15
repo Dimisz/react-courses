c30d393b3eebd6d6fa61ebea765d4a230fa94bec
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-15 12:01:18 +0800

    Implemented my version of lottery

--- colt/state-examples/src/App.js
+++ colt/state-examples/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 // import ScoreKeeper from './components/ScoreKeeper';
-import Ball from './components/Ball';
-
+// import Ball from './components/Ball';
+import Lottery from './components/Lottery';
 class App extends React.Component {
   render(){
     return(
-      <Ball num={1}/>
+      <Lottery />
     );
   }
 }
---
+++ colt/state-examples/src/components/Lottery.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Lottery.css';
+import Ball from './Ball';
+
+class Lottery extends React.Component {
+  static defaultProps = {
+    title: 'Lotto',
+    maxBalls: 6,
+    maxNum: 40
+  }
+
+  constructor(props){
+    super(props);
+    this.state = { nums: [] };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    const numBalls = Math.floor(Math.random() * this.props.maxBalls) + 1;
+    const balls = [];
+    for(let i = 0; i < numBalls; i++){
+      balls.push(Math.floor(Math.random() * this.props.maxNum) + 1);
+    }
+    this.setState((st) => {
+      return {nums: balls};
+    })
+  }
+
+  render(){
+    return(
+      <section className='lottery'>
+        <h1>{this.props.title}</h1>
+        <div>
+          {this.state.nums.map((num) => {
+            return <Ball key={num} num={num} />
+          })}
+        </div>
+        <button onClick={this.handleClick}>Generate</button>
+      </section>
+    )
+  }
+}
+
+export default Lottery;
