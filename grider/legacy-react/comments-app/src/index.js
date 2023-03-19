193feb9caf07de80e3b48e12865c735aa23dfe87
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-19 13:41:08 +0800

    Completed the app

--- grider/legacy-react/comments-app/src/index.js
+++ grider/legacy-react/comments-app/src/index.js
@@ -3,28 +3,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 const App = () => {
   return(
     <div className='ui container comments'>
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 2:32PM" 
-        text="Great post!"
-        image={faker.image.image()}
-      />
-      <CommentDetail 
-        author="Mike" 
-        timeAgo="Today at 4:34PM" 
-        text="Looks good"
-        image={faker.image.image()}
-      />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 7:53AM" 
-        text="Couldn't be better"
-        image={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 2:32PM" 
+          text="Great post!"
+          image={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Mike" 
+          timeAgo="Today at 4:34PM" 
+          text="Looks good"
+          image={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 7:53AM" 
+          text="Couldn't be better"
+          image={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
