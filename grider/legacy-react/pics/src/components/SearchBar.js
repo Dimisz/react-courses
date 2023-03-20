75b34c2207b5098e526b4020f602156a0a64263e
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-20 12:02:55 +0800

    Scaffolded the app

---
+++ grider/legacy-react/pics/src/components/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+  
+
+  onInputChange(event){
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <div className='ui segment'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
+          <div className='field'>
+            <label htmlFor="search">Image Search</label>
+            <input 
+              id="serach" 
+              name="search" 
+              type="text" 
+              onChange={this.onInputChange}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
