68d7bcd5bfe2ad017abaab8c13f7d98811b0c89c
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-21 17:14:22 +0800

    Completed the app

--- grider/legacy-react/youtube-search/src/App.js
+++ grider/legacy-react/youtube-search/src/App.js
@@ -3,6 +3,7 @@ import youtube from './api/youtube';
 
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 class App extends React.Component {
   constructor(props){
@@ -25,14 +26,23 @@ class App extends React.Component {
     const response = await youtube.get('/search', {
       params: { q: term }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
   }
 
   render(){
     return(
       <div className='ui container' style={{marginTop: '10px'}}>
         <SearchBar onFormSubmit={this.onFormSubmit} />
-        <VideoList videos={this.state.videos} onSelectVideo={this.onSelectVideo}/>
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList videos={this.state.videos} onSelectVideo={this.onSelectVideo}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
