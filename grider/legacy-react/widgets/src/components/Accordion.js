6da561a5b31fd73385d97150bd6073372acb0af4
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-22 10:54:07 +0800

    Completed the accodrdion widget

--- grider/legacy-react/widgets/src/components/Accordion.js
+++ grider/legacy-react/widgets/src/components/Accordion.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({items}) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const onTitleClick = (index) => {
-    console.log(`tittle ${index} clicked`);
+    setActiveIndex(index);
   }
+ 
   const renderedItems = items.map((item, i) => {
+    const active = i === activeIndex ? 'active' : '';
     return(
       <React.Fragment key={item.title}>
         <div 
-          className='title active' 
+          className={`title ${active}`} 
           onClick={() => onTitleClick(i)}
         >
           <i className='dropdown icon'></i>
           {item.title}
         </div>
-        <div className='content active'>
-        <p>{item.content}</p>
-        </div>
+          <div className={`content ${active}`}>
+          <p>{item.content}</p>
+          </div>
       </React.Fragment>
     );
   })
