287175f99dfdf6a4e6df544facbb177b643d79af
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-23 09:06:06 +0800

    Housekeeping

--- grider/legacy-react/widgets/src/components/Dropdown.js
+++ grider/legacy-react/widgets/src/components/Dropdown.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const renderedOptions = options.map((option) => {
     if(option !== selected){
       return(
-        <div key={option.value} className='item' onClick={() => onSelectedChange(option)}>
+        <div 
+            key={option.value} 
+            className='item' 
+            onClick={() => onSelectedChange(option)}
+        >
           {option.label}
         </div>
       );
@@ -14,10 +20,12 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
     <div className='ui form'>
       <div className='field'>
         <label className='label'>Select a color</label>
-        <div className='ui selection dropdown visible active'>
+        <div className={`ui selection dropdown ${isDropdownOpen ? 'visible active' : ''}`}
+             onClick={() => setIsDropdownOpen((isOpen) => {return !isOpen})}
+        >
           <i className='dropdown icon'></i>
           <div className='text'>{selected.label}</div>
-          <div className='menu visible transition'>
+          <div className={`menu ${isDropdownOpen ? 'visible transition' : ''}`}>
             {renderedOptions}
           </div>
         </div>
