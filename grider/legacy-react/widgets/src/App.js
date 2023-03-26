98a856b9d22b110453a1cc3a8b7c0cec3ce699ec
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-26 18:14:50 +0800

    Custom Route component

--- grider/legacy-react/widgets/src/App.js
+++ grider/legacy-react/widgets/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-
-// import Accordion from './components/Accordion';
-// import Dropdown from './components/Dropdown';
-// import Search from './components/Search';
+import Route from './components/Route';
+import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
+import Search from './components/Search';
 import Translate from './components/Translate';
 const items = [
   {
@@ -33,18 +33,46 @@ const dropdownOptions = [
     value: 'blue'
   }
 ]
+
+const showAccordion = () => {
+  if(window.location.pathname === '/'){
+    return <Accordion items={items} />;
+  }
+}
+
+const showSearch = () => {
+  if(window.location.pathname === '/search'){
+    return <Search />;
+  }
+}
+
+const showDropdown = () => {
+  if(window.location.pathname === '/dropdown'){
+    return <Dropdown />;
+  }
+}
+
+const showTranslate = () => {
+  if(window.location.pathname === '/translate'){
+    return <Translate />;
+  }
+}
 const App = () => {
   const [selected, setSelected] = useState(dropdownOptions[0]);
   return(
     <div>
-      {/* <Accordion items={items}/> */}
-      {/* <Search /> */}
-      {/* <Dropdown 
-          options={dropdownOptions}
-          selected={selected}
-          onSelectedChange={setSelected}
-      /> */}
-      <Translate />
+      <Route path='/'>
+        <Accordion items={items}/> 
+      </Route>
+      <Route path='/search'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
+        <Dropdown />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     </div>
   );
 }
