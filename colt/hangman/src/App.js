e9645bf17673eb124352a297dd0b61d5e7a9dd77
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-27 19:57:55 +0800

    Scaffolded the app

---
+++ colt/hangman/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className='app'>
+        Hangman
+      </div>
+    );
+  }
+}
+
+export default App;
