fa830d94f55a02c52d1692c0c11daf9d704dbd06
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-29 18:37:13 +0800

    Scaffolded the app

---
+++ colt/lights-out/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className='App'>Lights out</div>
+    );
+  }
+}
+
+export default App;
