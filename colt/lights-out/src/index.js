a268b6333311047d8ecb3b1bf4035398522504dc
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-30 17:00:47 +0800

    Housekeeping

--- colt/lights-out/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-import App from './App';
-
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el);
-root.render(<App />);
