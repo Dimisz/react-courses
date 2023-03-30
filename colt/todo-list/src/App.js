c883891f2f26276c996af6aa1ba2104b6976ba53
Vladimir Solovyov <leilyngale@icloud.com>
2023-03-30 18:21:11 +0800

    Scaffolded the App

---
+++ colt/todo-list/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>Todo App</div>
+    );
+  }
+}
+
+export default App;
