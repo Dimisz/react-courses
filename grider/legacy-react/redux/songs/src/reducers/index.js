65f2ed9433f1c1c31f672401911efdde79e6a02e
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-09 20:12:24 +0800

    Added a redux structure to the project

---
+++ grider/legacy-react/redux/songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '3:43' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want It That Way', duration: '1:45' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+  if(action.type === 'SONG_SELECTED'){
+    return action.payload;
+  }
+
+  return selectedSong;
+};
