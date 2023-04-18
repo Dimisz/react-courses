dd6513e32ca89da02d4689a7d128deebe78674c7
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-18 12:58:55 +0800

    Completed the first redux app

--- grider/legacy-react/redux/songs/src/components/SongList.js
+++ grider/legacy-react/redux/songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
   constructor(props){
@@ -12,7 +13,10 @@ class SongList extends React.Component {
     return this.props.songs.map((song) => {
       return (<div className='item' key={song.title}>
         <div className='right floated content'>
-          <button className='ui button primary'>
+          <button 
+              className='ui button primary'
+              onClick={() => this.props.selectSong(song)}
+            >
             Select
           </button>
         </div>
@@ -22,14 +26,20 @@ class SongList extends React.Component {
   }
 
   render(){
+    // console.log(this.props);
     return(
-      <div className='ui divided list'>{this.renderList()}</div>
+      <div className='ui divided list'>
+        {this.renderList()}
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
+  // console.log(state);
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong
+})(SongList);
