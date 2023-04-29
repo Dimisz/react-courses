07b1e878f97efbc413aedd0d1d5dfd98cc2143ef
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-29 09:44:07 +0800

    Scaffolded the app

--- grider/use-reducer-app/src/App.js
+++ grider/use-reducer-app/src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-
-import CounterPage from './components/CounterPage';
+import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [typedValue, setTypedValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount((prev) => {
+      return prev + Number(typedValue.trim());
+    });
+    setTypedValue('');
+  }
+
+  const increment = () => {
+    setCount((prev) => {
+      return prev + 1;
+    });
+  };
+
+  const decrement = () => {
+    setCount((prev) => {
+      return prev - 1;
+    });
+  };
+
   return(
-    <CounterPage initialCount={100} />
-  );
+    <main>
+      <h1>Counter App</h1>
+      <h2>Count: {count}</h2>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="number" 
+          value={typedValue}
+          onChange={(e) => {setTypedValue(e.target.value)}}
+        />
+        <button type="submit">Add a lot</button>
+      </form>
+    </main>
+  )
 }
 
 export default App;
--- grider/use-reducer-app/src/index.js
+++ grider/use-reducer-app/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-
 import App from './App';
 
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el);
-root.render(<App />);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App/>);
