25169131a2a5ab1a82cd545d6ce3ca6c2a9cc54b
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-30 11:28:12 +0800

    Scaffolded the app

---
+++ grider/rtk-intro-playlists-app/src/index.js
@@ -0,0 +1,8 @@
+import "bulma/css/bulma.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
