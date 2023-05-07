6960d119473376c5cfaea42ba955980ea28cba35
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-07 15:56:35 +0800

    Housekeeping

--- react-router-tut/official-tut/src/main.jsx
+++ react-router-tut/official-tut/src/main.jsx
@@ -6,14 +6,22 @@ import {
 } from 'react-router-dom';
 import './index.css'
 
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 import ErrorPage from './error-page';
+import Contact from './routes/contacts';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root/>,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />
+      },
+    ]
   },
 ]);
 
