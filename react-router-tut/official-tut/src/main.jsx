458d58c258557939ab3181b449e2cdd59ccd5240
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-07 13:15:04 +0800

    Scaffolded official react-router tutorial

---
+++ react-router-tut/official-tut/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import './index.css'
+
+import Root from './routes/root';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
