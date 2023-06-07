6bd1deeb51a5e0e9b7aad2f61f4df9b5a232ad5f
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-07 20:08:10 +0800

    My take on the ap

--- schmedtmann/steps/src/App.js
+++ schmedtmann/steps/src/App.js
@@ -1,6 +1,50 @@
+import { useState } from "react";
+
+const messages = [
+  'Learn React',
+  'Apply for jobs',
+  'Invest your new income'
+];
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const nextStepHandler = () => {
+    if(count < 2){
+      setCount((prev) => prev + 1);
+    }
+  }
+
+  const previousStepHandler = () => {
+    if(count > 0){
+      setCount((prev) => prev - 1);
+    }
+  }
+
   return(
-    <h1>Steps App</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div 
+          className={`${count === 0 ? 'active' : ''}`}
+        >1</div>
+        <div 
+          className={`${count === 1 ? 'active' : ''}`}
+        >2</div>
+        <div 
+          className={`${count === 2 ? 'active' : ''}`}
+        >3</div>
+      </div>
+      <p className="message">{messages[count]}</p>
+      <div className="buttons">
+        <button 
+          onClick={previousStepHandler}
+          className="active"
+        >Previous</button>
+        <button 
+          onClick={nextStepHandler}
+          className="active">Next</button>
+      </div>
+    </div>
   );
 }
 
