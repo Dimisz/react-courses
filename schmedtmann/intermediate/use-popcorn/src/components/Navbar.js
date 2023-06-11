a575efad9bb980f6879c9d4ab03483df3e1c64c7
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-11 18:15:23 +0800

    Extract Navbar to a separate component

---
+++ schmedtmann/intermediate/use-popcorn/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+const Navbar = ({movies}) => {
+  const [query, setQuery] = useState("");
+
+  return(
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p className="num-results">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    </nav>
+  )
+}
+
+export default Navbar;
