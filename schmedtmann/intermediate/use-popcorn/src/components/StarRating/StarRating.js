ba84e43a115285336ef5f72ae0d7114b674f8b9e
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-13 23:06:41 +0800

    Custo state setter

--- schmedtmann/intermediate/use-popcorn/src/components/StarRating/StarRating.js
+++ schmedtmann/intermediate/use-popcorn/src/components/StarRating/StarRating.js
@@ -11,19 +11,21 @@ const starContainerStyle = {
   // gap: '4px'
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0'
-};
 
-const starStyle = {
-  width: '48px',
-  height: '48px',
-  display: 'block',
-  cursor: 'pointer'
-}
-
-const Star = ({onClick, isFull, onMouseEnter, onMouseLeave}) => {
+const Star = ({
+    onClick, 
+    isFull, 
+    onMouseEnter, 
+    onMouseLeave,
+    color, 
+    size
+  }) => {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: 'block',
+    cursor: 'pointer'
+  }
   return(
     <span 
       onMouseEnter={onMouseEnter}
@@ -35,8 +37,8 @@ const Star = ({onClick, isFull, onMouseEnter, onMouseLeave}) => {
       {isFull && (<svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path
             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -47,7 +49,7 @@ const Star = ({onClick, isFull, onMouseEnter, onMouseLeave}) => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke="#000"
+        stroke={color}
       >
         <path
           strokeLinecap="round"
@@ -61,26 +63,46 @@ const Star = ({onClick, isFull, onMouseEnter, onMouseLeave}) => {
   );
 }
 
-const StarRating = ({maxRating}) => {
-  const [rating, setRating] = useState(0);
+const StarRating = ({
+  maxRating=5, 
+  color="#fcc419", 
+  size=36,
+  defaultRating=0, 
+  className,
+  onSet
+}) => {
+  const [rating, setRating] = useState(defaultRating);
   const [hoverRating, setHoverRating] = useState(0);
 
-  const numStars = Number(maxRating) || 5;
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    fontSize: `${size}px`,
+    color: color
+  };
+
+  const setRatingHandler = (rating) => {
+    setRating(rating);
+    onSet(rating);
+  }
+
   return(
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
-        {Array.from({length: numStars}, (_, i) => {
+        {Array.from({length: maxRating}, (_, i) => {
           return(
           <Star 
             key={i} 
+            color={color}
+            size={size}
             isFull={hoverRating > 0 ? (i + 1) <= hoverRating: (i + 1) <= rating}
-            onClick={() => setRating(i + 1)} 
+            onClick={() => setRatingHandler(i + 1)} 
             onMouseEnter={() => setHoverRating(i + 1)}
             onMouseLeave={() => setHoverRating(0)}
           />);
         })}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{hoverRating || rating || ""}</p>
     </div>
   );
 }
--- schmedtmann/intermediate/use-popcorn/src/index.js
+++ schmedtmann/intermediate/use-popcorn/src/index.js
@@ -7,5 +7,20 @@ import StarRating from './components/StarRating/StarRating';
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 // root.render(<App/>);
-root.render(<StarRating maxRating={10}/>);
+const firstRatingSetter = (rating) => {
+  console.log(`from first outer setter, rating: ${rating} has been set.`)
+}
+const secondRatingSetter = (rating) => {
+  console.log(`from second outer setter, rating: ${rating} has been set.`)
+}
+const thirdRatingSetter = (rating) => {
+  console.log(`from third outer setter, rating: ${rating} has been set.`)
+}
+root.render(
+  <>
+    <StarRating maxRating={10} defaultRating={6} onSet={firstRatingSetter}/>
+    <StarRating color="red" size={24} onSet={secondRatingSetter}/>
+    <StarRating maxRating={14} size={15} color="blue" onSet={thirdRatingSetter}/>
+  </>
+  );
 
