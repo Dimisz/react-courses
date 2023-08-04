0859be11f77e4a69db29b9f5a989603915214009
Vladimir Solovyov <leilyngale@icloud.com>
2023-08-04 18:48:14 +0800

    Set up redux store

--- grider/cars/src/index.js
+++ grider/cars/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App/>);
+root.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  );
---
+++ grider/cars/src/store/slices/carsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+
+const carsSlice = createSlice({
+  name: 'cars',
+  initialState: [],
+  reducers: {
+    changeSearchTerm(state, action){
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action){
+      state.cars.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    }, 
+    removeCar(state, action){
+      const updated = state.cars.filter((car) => {
+        return car.id !== action.payload
+      });
+      state.cars = updated;
+    }
+  }
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
