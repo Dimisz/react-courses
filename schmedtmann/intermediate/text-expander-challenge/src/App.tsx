bda3759258ffeb5e3fbdf2fc3443287cbe1127c3
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-21 23:56:06 +0800

    Housekeeping

--- schmedtmann/intermediate/text-expander-challenge/src/App.tsx
+++ schmedtmann/intermediate/text-expander-challenge/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TextExpander from './TextExpander'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
