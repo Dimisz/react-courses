fb99ffca46475c4cedb3c733e250bbcc94ac6d9e
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-21 20:58:34 +0800

    Create a resusable StarRating component

--- schmedtmann/intermediate/use-popcorn/src/components/star-rating/StarRating.tsx
+++ schmedtmann/intermediate/use-popcorn/src/components/star-rating/StarRating.tsx
@@ -21,17 +21,25 @@ const textStyle = {
 };
 
 const StarRating = ({ maxRating = 4 }: Props) => {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(3);
+  const [tempRating, setTempRating] = useState(0);
   return(
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({length: maxRating}, (_, i) => {
-          return(
-            <Star key={i} handleClick={() => setRating(i+1)}/>
+            return(
+              <Star 
+                key={i} 
+                handleClick={() => setRating(i+1)}
+                isFilled={ tempRating ? tempRating >= (i + 1) : (i + 1) <= rating }
+                onHoverIn={ () => setTempRating(i+1)}
+                onHoverOut={() => setTempRating(0)}
+              />
+            )}
           )
-        })}
+        }
       </div>
-      <p style={textStyle}>{rating || ''}</p>
+      <p style={textStyle}>{tempRating || rating || ''}</p>
     </div>
   );
 }
